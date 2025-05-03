86769a986178fa68bb94cc2650ec836d22c36ca2
Jose Adrian <kerpador@gmail.com>
2025-05-02 22:50:50 -0600

    possible implementation of numpy

--- src/screw/vector.py
+++ src/screw/vector.py
@@ -1,3 +1,4 @@
+import numpy as np
 class Vectors:
     """
     The class vector has:
@@ -18,14 +19,14 @@ class Vectors:
         It requires an n quantity of elements
         """
         if len(v)==1 and isinstance(v[0],(list,tuple)):
-            self.v=list(v[0])
+            self.v=np.array(v[0],dtype=float)
         else:
-            self.v=list(v)
+            self.v=np.array((v),dtype=float)
         if isinstance(v[0],(set)):
             print("It is not recommendable use sets")
     #Enables the len function for the object
     def __len__(self):
-        return len(self.v)
+        return self.v.size
     #enables the funciton to obtain values due an index
     def __getitem__(self,index):
         return self.v[index]
@@ -39,13 +40,13 @@ class Vectors:
     def __repr__(self):
         return f"Vector({', '.join(str(c) for c in self.v)})"
     #define the sum
-    def __add__(self,other):
-        if isinstance(other,Vectors):
-            if (len(other)!=len(self.v)):
-                raise ValueError("Vector must be at the same length")
-            sumed=[self.v[i]+other.v[i] for i in range(len(self.v))]
-            return Vectors(*sumed)
-        return NotImplemented
+    def __add__(self, other):
+        if isinstance(other, Vectors):
+            if len(other.v) != len(self.v):
+                raise ValueError("Vector must be the same length")
+            return Vectors(*tuple(self.v + other.v))
+        else:
+            return NotImplemented
     def __radd__(self,other):
         if other==0:
             return self
@@ -54,20 +55,20 @@ class Vectors:
         if isinstance(other,Vectors):
             if (len(other.v)!=len(self.v)):
                 raise ValueError("Vector must be at the same length")
-            subs=[self.v[i]-other.v[i] for i in range(len(self.v))]
-            return Vectors(*subs)
+            return Vectors(*tuple(self.v-other.v))
         return NotImplemented
     def __rsub__(self,other):
-            
         if other==0:
-            return Vectors(*[-x for x in self.v])
-        return self.__sub__(other)
+            return Vectors(*(-self.v))
+        return NotImplemented
     def UnitaryVector(self):
         """
         The calculation of the unitary vector it goes with itself
         """
-        denominator=sum([u**2 for u in self.v])
-        return [(1/(denominator)**(1/2))*u for u in self.v]
+        norm=np.linalg.norm(self.v)
+        if norm==0:
+                raise ValueError("Cannot normalize the vector")
+        return Vectors(*tuple(self.v/norm))
     def DirectionalVector(self,v1):
         """
         You require another vector
@@ -81,40 +82,31 @@ class Vectors:
         if isinstance(v1,Vectors) and isinstance(v2,Vectors):
             if(len(v1.v)!=len(v2.v)):
                 raise ValueError("Vectors must be at the same length")
-            mult=sum([v2.v[i]*v1.v[i] for i in range(len(v1.v))])
-            return mult
+            return float(np.dot(v1.v,v2.v))
         return NotImplemented
     def __xor__(self,other):
         if isinstance(other,Vectors):
-            if(len(other.v)!=3 or len(self.v)!=3):
+            if self.v.shape != (3,) or other.v.shape != (3,):
                 raise ValueError("Vectors must be 3-dimension")
-
-            return Vectors(
-                self.v[1]*other.v[2]-self.v[2]*other.v[1],
-                self.v[2]*other.v[0]-self.v[0]*other.v[2],
-                self.v[0]*other.v[1]-self.v[1]*other.v[0]
-            ) 
+            return Vectors(*np.cross(self.v,other.v)) 
         return NotImplemented
     def __mul__(self,scalar):
         if (isinstance(scalar,(int,float))) :
-            return Vectors(*[scalar*i for i in self.v])
+            return Vectors(*(self.v*scalar))
         elif isinstance(scalar,Vectors):
             return self.DotProd(self,scalar)
         return NotImplemented
     def __rmul__(self,scalar):
         return self.__mul__(scalar)
-
-
-
     #equality
     def __eq__(self, v1):
         if not isinstance(v1,Vectors):
             return NotImplemented
-        return self.v==v1.v
-    def __neq__(self, v1):
+        return np.array_equal(self.v,v1.v)
+    def __ne__(self, v1):
         if not isinstance(v1,Vectors):
             return NotImplemented
-        return not self.v==v1.v
+        return not np.array_equal(self.v,v1.v)
 
 
     #So at this point I have to put other class, called PLÜCKER COORDINATES
